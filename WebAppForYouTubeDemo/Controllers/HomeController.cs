18a0bd6fd4f7e62a4b2e35932bc4a776c7294340
zaheersani <sani.zaheer@gmail.com>
2019-06-20 15:59:31 +0500

    Add project files.

---
+++ WebAppForYouTubeDemo/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppForYouTubeDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.MyMsg = "I am sending something from About Action!";
+
+            return View();
+        }
+
+        public ActionResult Demo()
+        {
+            ViewBag.Message = "From Demo Action!";
+            return View("About");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
